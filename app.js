8fbb401755b5db4804be5ce90e26a58f07d330a4
Pablo Suarez <pa.suarezm@uniandes.edu.co>
2019-09-10 12:26:56 -0500

    Solo falta DELETE

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const MongoClient = require("mongodb").MongoClient;
 const express = require("express");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 var conn = MongoClient.connect("mongodb://localhost:27017",
 {useNewUrlParser: true, useUnifiedTopology: true});
 
-function getStudents(cllBack){
+function getCountries(cllBack){
     conn.then((client) => {
         client.db("poblaciones").collection("paises").find({}).toArray((err, data) => {
             cllBack(data);
@@ -13,14 +17,76 @@ function getStudents(cllBack){
     });
 }
 
+function getCountry(nombre, cllBack){
+    conn.then((client) => {
+        client.db("poblaciones").collection("paises").find({country: `${nombre}`}).toArray((err, data) => {
+            cllBack(data);
+        });
+    });
+}
+
+function postCountry(datosJSON, cllBack){
+    conn.then((client) => {
+        client.db("poblaciones").collection("paises").insertOne(datosJSON, cllBack());
+    });
+}
+
+function putCountry(nombre, datosJSON, cllBack){
+    conn.then((client) => {
+        client.db("poblaciones").collection("paises").updateOne({country: `${nombre}`}, { $set: datosJSON}, cllBack());
+    });
+}
+
+function deleteCountry(nombre, cllBack){
+    conn.then((client) => {
+        client.db("poblacioens").collection("paises").deleteOne({country: `${nombre}`}, cllBack());
+    });
+}
+
 app.get("/countries", (req, res) => {
-    getStudents((data) => {
-        res.json(data)
+    console.log("GET all countries");
+    getCountries((data) => {
+        res.json(data);
     });
 });
 
-app.get("/countries", (req, res) => {
+app.get("/countries/:id", (req, res) => {
+    var id = req.params.id;
+    console.log(`GET country ${id}`);
+    getCountry(id, (data) => {
+        res.json(data);
+    });
+});
 
+app.post("/countries", (req, res) => {
+    var rawBody = req.body;
+    console.log(`POST country ${rawBody.country}`);
+    postCountry(rawBody, () => {
+        getCountry(rawBody.country, (data) => {
+            res.json(data);
+        })
+    });
+});
+
+app.put("/countries/:id", (req, res) => {
+    var id = req.params.id;
+    var rawBody = req.body;
+    console.log(`PUT country ${id}`);
+    putCountry(id, rawBody, () => {
+        getCountry(id, (data) => {
+            res.json(data);
+        })
+    });
+});
+
+app.delete("/countries/:id", (req, res) => {
+    var id = req.params.id;
+    console.log(`DELETE country ${id}`);
+    deleteCountry(id, () => {
+        getCountries((data) => {
+            res.json(data);
+        });
+    });
 });
 
 app.listen(8080);
